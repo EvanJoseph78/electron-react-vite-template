fa4499e65ca6baab568ae6f7b74bb0fd97880523
evan <evandromariano49@gmail.com>
2025-05-24 22:00:37 -0300

    fix

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,43 +1,43 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 // import reactLogo from "./assets/react.svg";
 import "./App.css";
 import Menubar from "./components/menubar";
-import Tabs from "./components/tabs";
+// import Tabs from "./components/tabs";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
-  const [randomString, setRandomString] = useState("");
+  // const [randomString, setRandomString] = useState("");
 
-  useEffect(() => {
-    // Escutando o evento de string aleatória
-    // @ts-ignore
-    window.electron.subscribeStatistic((data) => {
-      setRandomString(data);
-    });
+  // useEffect(() => {
+  //   // Escutando o evento de string aleatória
+  //   // @ts-ignore
+  //   window.electron.subscribeStatistic((data) => {
+  //     setRandomString(data);
+  //   });
 
-    // Buscando a lista de usuários
-    const fetchUsers = async () => {
-      const users = await window.electron.user.getAll();
-      console.log(users);
-    };
+  //   // Buscando a lista de usuários
+  //   const fetchUsers = async () => {
+  //     const users = await window.electron.user.getAll();
+  //     console.log(users);
+  //   };
 
-    // função para criar um usuário
-    const createNewUser = async () => {
-      const user = {
-        nome: "Evan Joseph",
-        idade: 25,
-        profissao: "Developer",
-        favoriteColor: "Orange",
-      };
-      const newUser = await window.electron.user.create(user);
-      console.log(newUser);
-    };
+  //   // função para criar um usuário
+  //   const createNewUser = async () => {
+  //     const user = {
+  //       nome: "Evan Joseph",
+  //       idade: 25,
+  //       profissao: "Developer",
+  //       favoriteColor: "Orange",
+  //     };
+  //     const newUser = await window.electron.user.create(user);
+  //     console.log(newUser);
+  //   };
 
-    createNewUser();
+  //   createNewUser();
 
-    fetchUsers();
-  }, []);
+  //   fetchUsers();
+  // }, []);
 
   return (
     <div>
